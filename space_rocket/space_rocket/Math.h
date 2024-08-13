997f27a9d7196287d3ecc3dd441c5f19b9740466
horolock <horolock96@gmail.com>
2024-08-13 22:04:18 +0900

    Feat: Vector2

--- space_rocket/space_rocket/Math.h
+++ space_rocket/space_rocket/Math.h
@@ -51,3 +51,91 @@ namespace Math
 	inline float Sqrt(float value) { return sqrtf(value); }
 	inline float Fmod(float number, float denom) { return fmod(number, denom); }
 }
+
+class Vector2
+{
+public:
+	float x;
+	float y;
+
+	Vector2() : x(0.0f), y(0.0f) {}
+	explicit Vector2(float inX, float inY) : x(inX), y(inY) {}
+
+	void Set(float inX, float inY) {
+		x = inX;
+		y = inY;
+	}
+
+	friend Vector2 operator+(const Vector2& a, const Vector2& b) {
+		return Vector2(a.x + b.x, a.y + b.y);
+	}
+
+	friend Vector2 operator-(const Vector2& a, const Vector2& b) {
+		return Vector2(a.x - b.x, a.y - b.y);
+	}
+
+	friend Vector2 operator*(const Vector2& vec, float scaler) {
+		return Vector2(vec.x * scaler, vec.y * scaler);
+	}
+
+	friend Vector2 operator*(float scaler, const Vector2& vec) {
+		return Vector2(vec.x * scaler, vec.y * scaler);
+	}
+
+	Vector2& operator*=(float scaler) {
+		x *= scaler;
+		y *= scaler;
+		return *this;
+	}
+
+	Vector2& operator+=(const Vector2& right) {
+		x += right.x;
+		y += right.y;
+		return *this;
+	}
+
+	Vector2& operator-=(const Vector2& right) {
+		x -= right.x;
+		y -= right.y;
+		return *this;
+	}
+
+	float LengthSq() const {
+		return (x * x + y * y);
+	}
+
+	float Length() const {
+		return (Math::Sqrt(LengthSq()));
+	}
+
+	void Normalize() {
+		float length = Length();
+		x /= length;
+		y /= length;
+	}
+
+	static Vector2 Normalize(const Vector2& vec) {
+		Vector2 temp = vec;
+		temp.Normalize();
+		return temp;
+	}
+
+	static float Dot(const Vector2& a, const Vector2& b) {
+		return (a.x * b.x + a.y * b.y);
+	}
+
+	static Vector2 Lerp(const Vector2& a, const Vector2& b, float f) {
+		return Vector2(a + f * (b - a));
+	}
+
+	static Vector2 Reflect(const Vector2& v, const Vector2& n) {
+		return v - 2.0f * Vector2::Dot(v, n) * n;
+	}
+
+	/* Transform vector by matrix */
+	static const Vector2 Zero;
+	static const Vector2 UnitX;
+	static const Vector2 UnitY;
+	static const Vector2 NegativeUnitX;
+	static const Vector2 NegativeUnitY;
+};
