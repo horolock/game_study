4b14e234b0052148dbafb0cd5589c3b552158a1a
horolock <horolock96@gmail.com>
2024-08-13 22:32:36 +0900

    Feat: create getter, setter, component related APIs

--- space_rocket/space_rocket/Actor.h
+++ space_rocket/space_rocket/Actor.h
@@ -20,15 +20,29 @@ public:
 	void UpdateComponents(float deltaTime);
 	virtual void UpdateActor(float deltaTime);
 
-	/* Getter, Setter */
+	/* Getter */
 	const Vector2& GetPosition() const { return mPosition; }
+	float GetScale() const { return mScale; }
+	float GetRotation() const { return mRotation; }
+	class Game* GetGame() { return mGame; }
 
+	/* Setter */
+	void SetPosition(const Vector2& pos) { mPosition = pos; }
+	void SetScale(float scale) { mScale = scale; }
+	void SetRotation(float rotation) { mRotation = rotation; }
+
+	/* Component */
+	void AddComponent(class Component* component);
+	void RemoveComponent(class Component* component);
 
 private:
 	State mState;
 	Vector2 mPosition;
+
 	float mScale;
 	float mRotation;
+
 	std::vector<class Component*> mComponents;
+
 	class Game* mGame;
 };
