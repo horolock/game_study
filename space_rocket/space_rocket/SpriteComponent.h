27f95a672146fb297ed01198f5e95abbbc8f67bd
horolock <horolock96@gmail.com>
2024-08-15 22:33:42 +0900

    Feat: Sprite component

---
+++ space_rocket/space_rocket/SpriteComponent.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Component.h"
+#include "SDL.h"
+
+class SpriteComponent : public Component
+{
+public:
+	SpriteComponent(class Actor* owner, int drawOrder = 100);
+	~SpriteComponent();
+
+	virtual void Draw(SDL_Renderer* renderer);
+	virtual void SetTexture(SDL_Texture* texture);
+
+	int GetDrawOrder() const { return mDrawOrder; }
+	int GetTextureHeight() const { return mTextureHeight; }
+	int GetTextureWidth() const { return mTextureWidth; }
+
+protected:
+	SDL_Texture* mTexture;
+	int mDrawOrder;
+	int mTextureWidth;
+	int mTextureHeight;
+};
