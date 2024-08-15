b9d1822c38b91665af9f3df7c2dfb15f2c01c748
horolock <horolock96@gmail.com>
2024-08-15 22:49:16 +0900

    Feat: Anim sprite component

---
+++ space_rocket/space_rocket/AnimSpriteComponent.cpp
@@ -0,0 +1,37 @@
+#include "AnimSpriteComponent.h"
+#include "Math.h"
+
+AnimSpriteComponent::AnimSpriteComponent(class Actor* owner, int drawOrder = 100)
+	: SpriteComponent(owner, drawOrder),
+	mCurrentFrame(0.0f),
+	mAnimFPS(24.0f)
+{
+}
+
+void AnimSpriteComponent::Update(float deltaTime)
+{
+	SpriteComponent::Update(deltaTime);
+
+	if (mAnimTextures.size() > 0)
+	{
+		mCurrentFrame += mAnimFPS * deltaTime;
+
+		while (mCurrentFrame >= mAnimTextures.size())
+		{
+			mCurrentFrame -= mAnimTextures.size();
+		}
+
+		SetTexture(mAnimTextures[static_cast<int>(mCurrentFrame)]);
+	}
+}
+
+void AnimSpriteComponent::SetAnimTextures(const std::vector<SDL_Texture*>& textures)
+{
+	mAnimTextures = textures;
+
+	if (mAnimTextures.size() > 0)
+	{
+		mCurrentFrame = 0.0f;
+		SetTexture(mAnimTextures[0]);
+	}
+}
