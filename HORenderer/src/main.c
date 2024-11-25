c058105c303129eb4944b99f5dd41aff9809fad5
horolock <horolock96@gmail.com>
2024-11-25 22:59:07 +0900

    create vector3 array for cube

--- HORenderer/src/main.c
+++ HORenderer/src/main.c
@@ -1,6 +1,9 @@
 #include "display.h"
 #include "vector.h"
 
+#define N_POINTS    (9 * 9 * 9)
+
+vec3_t cube_points[N_POINTS]; 
 bool is_running = false;
 
 bool setup(void)
@@ -25,6 +28,17 @@ bool setup(void)
     if (!color_buffer_texture) {
         fprintf(stderr, "Error creating SDL Texture.\n");
         return false;
+    }
+
+    int point_count = 0;
+
+    for (float x = -1; x <= 1; x += 0.25) {
+        for (float y = -1; y <= 1; y += 0.25) {
+            for (float z = -1; z <= 1; z+= 0.25) {
+                vec3_t new_point = { .x = x, .y = y, .z = z};
+                cube_points[point_count++] = new_point;
+            }
+        }
     }
 
     return true;
