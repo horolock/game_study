c2fb502afec3438277ee5bcaeefc8167f7d189a1
horolock <horolock96@gmail.com>
2024-12-15 03:56:59 +0900

    Beautiful cube!

--- HORenderer/src/display.c
+++ HORenderer/src/display.c
@@ -90,7 +90,14 @@ void draw_line(int x0, int y0, int x1, int y1, uint32_t color)
         current_x += x_inc;
         current_y += y_inc;
     }
-} 
+}
+
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color)
+{
+    draw_line(x0, y0, x1, y1, color);
+    draw_line(x1, y1, x2, y2, color);
+    draw_line(x2, y2, x0, y0, color);
+}
 
 vec2_t project(vec3_t point)
 {
