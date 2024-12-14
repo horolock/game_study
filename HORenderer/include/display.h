c29ad4a9aad8dac87b2d490a068a39abae737220
horolock <horolock96@gmail.com>
2024-12-15 03:22:48 +0900

    Draw line feature

--- HORenderer/include/display.h
+++ HORenderer/include/display.h
@@ -24,6 +24,7 @@ bool initialize_window(void);
 void draw_grid(uint32_t color);
 void draw_pixel(int x, int y, uint32_t color);
 void draw_rectangle(int pos_x, int pos_y, int width, int height, uint32_t color);
+void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
 vec2_t project(vec3_t point);
 void render_color_buffer(void);
 void clear_color_buffer(uint32_t color);
--- HORenderer/src/display.c
+++ HORenderer/src/display.c
@@ -70,6 +70,28 @@ void draw_rectangle(int pos_x, int pos_y, int width, int height, uint32_t color)
     }
 }
 
+void draw_line(int x0, int y0, int x1, int y1, uint32_t color)
+{
+    int delta_x = (x1 - x0);
+    int delta_y = (y1 - y0);
+
+    int longest_side_length = (abs(delta_x) >= abs(delta_y) ? abs(delta_x) : abs(delta_y));
+
+    // Division is always problem with slow.
+    // Find how much we should increment in both x and y each step
+    float x_inc = delta_x / (float)longest_side_length;
+    float y_inc = delta_y / (float)longest_side_length;
+
+    float current_x = x0;
+    float current_y = y0;
+
+    for (int i = 0; i <= longest_side_length; i++) {
+        draw_pixel(round(current_x), round(current_y), color);
+        current_x += x_inc;
+        current_y += y_inc;
+    }
+} 
+
 vec2_t project(vec3_t point)
 {
     vec2_t projected_point = {
