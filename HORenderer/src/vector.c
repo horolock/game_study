a90b50613808d6f7ee39a2d60c037f1caebe6fe2
horolock <horolock96@gmail.com>
2024-12-20 00:14:30 +0900

    Feature vector mul, div

--- HORenderer/src/vector.c
+++ HORenderer/src/vector.c
@@ -12,7 +12,7 @@ vec2_t vec2_add(vec2_t a, vec2_t b)
         .x = a.x + b.x,
         .y = a.y + b.y
     };
-    
+
     return result;
 }
 
@@ -26,6 +26,25 @@ vec2_t vec2_sub(vec2_t a, vec2_t b)
     return result;
 }
 
+vec2_t vec2_mul(vec2_t a, float factor)
+{
+    vec2_t result = {
+        .x = a.x * factor,
+        .y = a.y * factor
+    };
+    
+    return result;
+}
+
+vec2_t vec2_div(vec2_t a, float factor)
+{
+    vec2_t result = {
+        .x = a.x / factor,
+        .y = a.y / factor
+    };
+    
+    return result;
+}
 
 float vec3_length(vec3_t v)
 {
@@ -49,6 +68,28 @@ vec3_t vec3_sub(vec3_t a, vec3_t b)
         .x = a.x - b.x,
         .y = a.y - b.y,
         .z = a.z - b.z
+    };
+
+    return result;
+}
+
+vec3_t vec3_mul(vec3_t a, float factor)
+{
+    vec3_t result = {
+        .x = a.x * factor,
+        .y = a.y * factor,
+        .z = a.z * factor
+    };
+
+    return result;
+}
+
+vec3_t vec3_div(vec3_t a, float factor)
+{
+    vec3_t result = {
+        .x = a.x / factor,
+        .y = a.y / factor,
+        .z = a.z / factor
     };
 
     return result;
