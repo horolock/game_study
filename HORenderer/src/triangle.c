6e1e5c6ffc887419d1abb910d60f82845feb18d6
horolock <horolock96@gmail.com>
2024-12-23 23:47:05 +0900

    fill triangle skeleton method

--- HORenderer/src/triangle.c
+++ HORenderer/src/triangle.c
@@ -1,3 +1,33 @@
 #include "triangle.h"
 
-// TODO: Create implementation for triangle.h functions
+void int_swap(int* a, int* b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color)
+{
+    // We need to start the vertices by y-coordinate ascending (y0 < y1 < y2)
+    if (y0 > y1) {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+    }
+
+    if (y1 > y2) {
+        int_swap(&y1, &y2);
+        int_swap(&x1, &x2);
+    }
+
+    if (y0 > y1) {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+    }
+
+    // TODO: Draw flat-bottom triangle
+    // fill_flat_bottom_triangle();
+
+    // TODO: Draw flat-top triangle
+    // fill_flat_top_triangle();
+}
