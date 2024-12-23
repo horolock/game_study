11f19c1248d3b5a6240833abe4a24207e717ca63
horolock <horolock96@gmail.com>
2024-12-23 23:51:27 +0900

    declare fill bottom, top triangle

--- HORenderer/src/triangle.c
+++ HORenderer/src/triangle.c
@@ -7,6 +7,14 @@ void int_swap(int* a, int* b)
     *b = tmp;
 }
 
+void fill_flat_bottom_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+
+}
+
+void fill_flat_top_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+
+}
+
 void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color)
 {
     // We need to start the vertices by y-coordinate ascending (y0 < y1 < y2)
@@ -25,9 +33,13 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
         int_swap(&x0, &x1);
     }
 
+    // Calculate the new vertex (Mx, My) using triangle similarity
+    int My = y1;
+    int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
+
     // TODO: Draw flat-bottom triangle
-    // fill_flat_bottom_triangle();
+    fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My);
 
     // TODO: Draw flat-top triangle
-    // fill_flat_top_triangle();
+    fill_flat_top_triangle(x1, y1, Mx, My, x2, y2);
 }
